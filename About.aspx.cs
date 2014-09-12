cca3cfda8fc86863397bffd404ece9cd4783b958
Nick Karastamatis <nkarastamatis@gmail.com>
2014-09-12 12:26:56 -0400

    Committee Data (Part 1)

--- About.aspx.cs
+++ About.aspx.cs
@@ -4,11 +4,15 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Xml.Schema;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Xml.Serialization.Advanced;
+using System.Xml.Serialization.Configuration;
 public partial class About : Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        var c = new Committee();      
     }
 }
---
+++ App_Code/Committee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Committee
+/// </summary>
+public class Committee
+{
+    public string CommitteeName {get; set;}
+    public string Description {get; set;}
+
+	public Committee()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
